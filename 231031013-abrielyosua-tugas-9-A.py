eb930da29789f14fad6af16d84eaf2fd2ef9a53b
Abelzz <115933888+Belzzcoding@users.noreply.github.com>
2023-12-18 21:20:50 +0800

    Add files via upload

---
+++ 231031013-abrielyosua-tugas-9-A.py
@@ -0,0 +1,11 @@
+biodata = {
+    "nama" : "Abriel Yosua Nathanael Leskona",
+    "TTL" : "Makassar, 03 Desember 2004",
+    "Hobi" : ["Olahraga", "Dengar musik", "Membaca"],
+    "Pekerjaan" : "Mahasiswa", 
+    "Kampus" : "Institut Teknologi B.J. Habibie",
+    "Semester" : "Ganjil"
+}
+
+for i in biodata:
+    print(biodata[i])
